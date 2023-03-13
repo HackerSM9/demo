1647f85a9dfeec85f17afe13a679ac9afe7b14e7
HackerSM9 <sm9hacking@gmail.com>
2023-03-13 20:27:09 +0530

    new file:   script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// how to make spinner in javascript
+var spinner = new Spinner({
+    radius: 40,
+    length: 40,
+    width: 40,
+    color: '#000',
+    trail: 60,
+    speed: 1.5,
+    top: '50%',
+    left: '50%'
+    });
+    
