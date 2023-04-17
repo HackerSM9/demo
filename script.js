9dcdbb6aed51f0728ca6f94342a804def406a997
HackerSM9 <sm9hacking@gmail.com>
2023-04-17 16:20:16 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-// how to make spinner in javascript
-var spinner = new Spinner({
-    radius: 40,
-    length: 40,
-    width: 40,
-    color: '#000',
-    trail: 60,
-    speed: 1.5,
-    top: '50%',
-    left: '50%'
-    });
-    
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hackersm9 App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hackersm9 App'),
+        ),
+        body: Center(
+          child: Text(
+            'Hackersm9 here',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
